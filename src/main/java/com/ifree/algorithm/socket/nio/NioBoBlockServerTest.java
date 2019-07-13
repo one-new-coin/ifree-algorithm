081091d4f8001248aee26f51136ec79ec6669de5
冯宇鹏 <20641453@qq.com>
2019-07-13 22:35:28 +0800

    update

--- src/main/java/com/ifree/algorithm/socket/nio/NioBoBlockServerTest.java
+++ src/main/java/com/ifree/algorithm/socket/nio/NioBoBlockServerTest.java
@@ -10,6 +10,27 @@ import java.util.Iterator;
 
 import org.junit.Test;
 
+
+/*
+ * 一、使用 NIO 完成网络通信的三个核心：
+ * 
+ * 1. 通道（Channel）：负责连接
+ * 		
+ * 	   java.nio.channels.Channel 接口：
+ * 			|--SelectableChannel
+ * 				|--SocketChannel
+ * 				|--ServerSocketChannel
+ * 				|--DatagramChannel
+ * 
+ * 				|--Pipe.SinkChannel
+ * 				|--Pipe.SourceChannel
+ * 
+ * 2. 缓冲区（Buffer）：负责数据的存取
+ * 
+ * 3. 选择器（Selector）：是 SelectableChannel 的多路复用器。用于监控 SelectableChannel 的 IO 状况
+ * 
+ */
+
 public class NioBoBlockServerTest {
 
 	@Test
